b16e4dcbbb47324af139298fdef13c10c558c27f
NastyaNikolenko <115963120+NastyaNikolenko@users.noreply.github.com>
2022-10-21 22:58:59 +0700

    Update rcis_ep_task_one.dart

--- rcis_ep_task_one/bin/rcis_ep_task_one.dart
+++ rcis_ep_task_one/bin/rcis_ep_task_one.dart
@@ -1,5 +1,32 @@
-import 'package:rcis_ep_task_one/rcis_ep_task_one.dart' as rcis_ep_task_one;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${rcis_ep_task_one.calculate()}!');
+void main(){
+  //zadanie 1 pr 1.1
+
+  List<int> massive = [];
+
+  List<int> massiveRandom(massive){
+    for (int i = 0; i < 10; i++){
+      int nums = Random().nextInt(20) + 20;
+      massive.add(nums);
+    }
+    return massive;
+  } 
+  massive = massiveRandom(massive);
+
+  int minMassiveNumber = massive[0];
+  int index = 0;
+
+  for (int i = 0; i < 10; i++){
+    if (massive[i] < minMassiveNumber){
+      minMassiveNumber = massive[i];
+      index = i;
+    }
+  }
+
+
+  print('Массив, заплненный случайными числами от 20 до 40: $massive');
+  print('Минимальное число массива: $minMassiveNumber');
+  print('Номер минимального числа массива: $index');
 }
