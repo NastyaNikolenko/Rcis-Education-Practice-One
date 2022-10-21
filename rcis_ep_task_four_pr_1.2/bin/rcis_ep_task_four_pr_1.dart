6595bc5653c9e82b22ecfb70d5eb486a1b79d7e2
NastyaNikolenko <115963120+NastyaNikolenko@users.noreply.github.com>
2022-10-21 23:31:57 +0700

    Add files via upload

---
+++ rcis_ep_task_four_pr_1.2/bin/rcis_ep_task_four_pr_1.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  //zadanie 4 pr 1.2
+
+  List<List<int>> temperature;
+  temperature = List.generate(12, (_) => List.generate(30, (_) => Random().nextInt(60) - 30));
+
+  print('Температура каждого дня в каждом месяце года: $temperature');
+
+  List temperatureMoth(List<List<int>> temperature){
+
+    List<int> massive = List.filled(12, 0);
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 30; j++){
+          massive[i] += temperature[i][j];
+        }
+      massive[i] = massive[i] ~/ 30;
+      
+      }
+    return massive;
+  }
+  
+  print('Средняя температура каждого месяца: ${temperatureMoth(temperature)}');
+  print('Отсортированный массив средних температур каждого месяца: ${temperatureMoth(temperature)..sort()}');
+}
